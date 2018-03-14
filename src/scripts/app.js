23f82321666f0777c37a72847431497a299bd39f
Cody Gordon <cjgordon@gmail.com>
2018-03-14 14:47:19 -0400

    move donate-specific scripts to donate.js & serve to demo pages

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,12 +8,6 @@ import HomeHero from './components/homeHero';
 import SignForm from './components/signForm';
 import PetitionComments from './components/petitionComments';
 import PetitionCard from './components/petitionCard';
-import DonateReceipt from './components/donateReceipt';
-import DonateShortForm from './components/donateShortForm';
-import DonateFullForm from './components/donateFullForm';
-import DonateQuickPay from './components/donateQuickPay';
-import DonateBoxedForm from './components/donateBoxedForm';
-import PriceSelectGroup from './components/priceSelectGroup';
 
 $(document).ready(function () {
   Nav.init();
@@ -26,13 +20,4 @@ $(document).ready(function () {
   SignForm.init();
   PetitionComments.init();
   PetitionCard.init();
-  /* NOTE: the donate page logic now lives in:
-    ak-template-set/template_components/donate/scripts.html
-
-    DonateReceipt.init();
-    DonateShortForm.init();
-    DonateFullForm.init();
-    DonateBoxedForm.init();
-    DonateQuickPay.init();
-    PriceSelectGroup.init(); */
 });
